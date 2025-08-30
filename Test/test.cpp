1ee2c8c101b408c7fad2308cfebefbd55aa8943f
suryahub1994 <suryasispaul2001@gmail.com>
2025-08-31 00:02:10 +0400

    Adding some more asserts and also clang-format

--- Test/test.cpp
+++ Test/test.cpp
@@ -1,9 +1,2 @@
 #include "../src/adarsha_test.hpp"
-int main()
-{
-    TEST("suryasis0", [](){ return Adarsha::assert(1==2, "Not equal"); });
-    TEST("suryasis1", [](){ return Adarsha::assertEqual(1, 2, "Not equal"); });
-    TEST("suryasis2", [](){ return Adarsha::assertNotEqual(1, 2, "Not equal"); });
-    TEST("suryasis3", [](){ return Adarsha::assertLess(1, 2, "Not equal"); });
-    TEST("suryasis4", [](){ return Adarsha::assertGreater(1, 2, "Not equal"); });
-}
+int main() {}
--- src/adarsha_test.hpp
+++ src/adarsha_test.hpp
@@ -2,58 +2,84 @@
 #include <map>
 
 #define TEST(name, value) Adarsha::TestRepository::registerTest(name, value)
+#define RUN_ALL() Adarsha::TestRepository::runAll();
+namespace Adarsha {
+class TestRepository {
+private:
+  inline static std::map<std::string, std::function<bool()>> repository{};
 
-namespace Adarsha
-{
-    class TestRepository 
-    {
-        private:
-             inline static std::map<std::string, std::function<bool()>> repository{};
-        public:
-            static void registerTest(const std::string &testName, std::function<bool()> func) 
-            {
-                repository[testName] = func;
-            }
-    }; 
+public:
+    static void registerTest(const std::string &testName,
+                            std::function<bool()> func) {
+        repository[testName] = func;
+    }
+    static void runAll() {
+        for (auto [name, func] : repository) {
+        if (func()) {
+            std::cout << "Passed " << name << std::endl;
+        } else {
+            std::cout << "Failed " << name << std::endl;
+        }
+        }
+    }
+    };
+
+    inline constexpr double DEFAULT_EPS = 1e-9;
 
     bool assert(bool condition, const std::string message) {
-        if(condition)
-        {
-            return true;
-        }
-        std::cout<<message<<std::endl;
+    if (condition) {
+        return true;
+    }
+    std::cout << message << std::endl;
+    return false;
+    }
+    template <typename T>
+    bool assertEqual(const T &a, const T &b, const std::string &message) {
+    if (a == b) {
+        return true;
+    }
+    std::cout << message << std::endl;
+    return false;
+    }
+    template <typename T>
+    bool assertNotEqual(const T &a, const T &b, const std::string &message) {
+    if (a != b) {
+        return true;
+    }
+    std::cout << message << std::endl;
+    return false;
+    }
+    template <typename T>
+    bool assertLess(const T &a, const T &b, const std::string &message) {
+    if (a < b) {
+        return true;
+    }
+    std::cout << message << std::endl;
+    return false;
+    }
+    template <typename T>
+    bool assertGreater(const T &a, const T &b, const std::string &message) {
+    if (a > b) {
+        return true;
+    }
+    std::cout << message << std::endl;
+    return false;
+    }
+
+    template <typename T>
+    bool assertNear(const T &a, const T &b, const double eps = DEFAULT_EPS,
+                    const std::string &message = "") {
+    if (abs(a - b) < eps) {
+        return true;
+    }
+    std::cout << message << std::endl;
+    return false;
+    }
+    template <typename P> bool assertNull(P p, const std::string &msg = "") {
+    if (p != nullptr) {
         return false;
     }
-    template<typename T> bool assertEqual(const T& a, const T& b, const std::string& message) {
-        if(a == b)
-        {
-            return true;
-        }
-        std::cout<<message<<std::endl;
-        return false;
-    } 
-    template<typename T> bool assertNotEqual(const T& a, const T& b, const std::string& message) {
-        if(a != b)
-        {
-            return true;
-        }
-        std::cout<<message<<std::endl;
-        return false;
-    } 
-    template<typename T> bool assertLess(const T& a, const T& b, const std::string& message) {
-        if(a < b)
-        {
-            return true;
-        }
-        std::cout<<message<<std::endl;
-        return false;
+    std::cout << message << std::endl;
+    return true;
     }
-    template<typename T> bool assertGreater(const T& a, const T& b, const std::string& message) {
-        if(a > b)
-        {
-            return true;
-        }
-        std::cout<<message<<std::endl;
-        return false;
-    } 
-}
+} // namespace Adarsha
